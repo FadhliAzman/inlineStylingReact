0d11e42a63bad60647726e9961f61342abbf942d
newuser <newuser@Engkus.local>
2023-08-19 10:56:15 +1000

    inlineStylingReact

---
+++ src/App.jsx
@@ -0,0 +1,35 @@
+import HelloReacts from "./components/helloReacts";
+
+function App() {
+  return (
+    <>
+      <HelloReacts />
+      <HelloReacts />
+
+      <p
+        style={{
+          backgroundColor: "blue",
+          color: "white",
+          fontSize: "36px",
+          padding: "24px",
+          borderRadius: 20,
+        }}
+      >
+        hello react
+      </p>
+      <p
+        style={{
+          backgroundColor: "blue",
+          color: "white",
+          fontSize: "36px",
+          padding: "24px",
+          borderRadius: 20,
+        }}
+      >
+        hello react
+      </p>
+    </>
+  );
+}
+
+export default App;
---
+++ src/components/HelloReacts.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const HelloReacts = () => {
+  return (
+    <p
+      style={{
+        backgroundColor: "blue",
+        color: "white",
+        fontSize: "36px",
+        padding: "24px",
+        borderRadius: 20,
+      }}
+    >
+      hello react
+    </p>
+  );
+};
+
+export default HelloReacts;
